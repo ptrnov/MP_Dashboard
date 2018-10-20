8b1e4ca922ec37904ef60a057e4134935964db9d
ptrnov <ptr.nov@gmail.com>
2018-10-20 21:57:01 +0700

    update

--- src/pages/dsh6-home/dsh6-home.ts
+++ src/pages/dsh6-home/dsh6-home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController,ModalController,IonicPage, NavController, NavParams } from 'ionic-angular';
 import {SettingsPage} from "../settings/settings";
 import { SelectSearchableComponent  } from 'ionic-select-searchable';
 import { DashboardAllProvider } from "../../providers/dashboard-all/dashboard-all";
 import { DatabaseProvider } from '../../providers/database/database';
 import * as Highcharts from "highcharts";
 import addMore from "highcharts/highcharts-more";
+import {Dsh7ModalFlowPage} from '../dsh7-modal-flow/dsh7-modal-flow';
 
 // var aryListMitra=[];
 
@@ -35,6 +36,7 @@ var clk_spArea2;
 var clk_spArea3;
 var clk_spArea4;
 var charting;
+var dsh6_charting;
 @IonicPage()
 @Component({
   selector: 'page-dsh6-home',
@@ -44,12 +46,37 @@ export class Dsh6HomePage {
   // ports: Port[];
   port: Port;
   aryListMitra: Port[];
+  public aryValueMitra;
+  public aryb2sArea1;
+  public aryb2sArea2;
+  public aryb2sArea3;
+  public aryb2sArea4;
+    public aryCoreArea1;
+    public aryCoreArea2;
+    public aryCoreArea3;
+    public aryCoreArea4;
+  public aryMicroArea1;
+  public aryMicroArea2;
+  public aryMicroArea3;
+  public aryMicroArea4;
+    public arySpecialArea1;
+    public arySpecialArea2;
+    public arySpecialArea3;
+    public arySpecialArea4;
+
+
   constructor(
       public navCtrl: NavController,
       public navParams: NavParams,
       private dashboarAll: DashboardAllProvider,
       private database: DatabaseProvider,
+      public modalCtrl: ModalController,
+      public alertCtrl: AlertController,
   ) {
+      this.dashboarAll.postDatax("Mobile_Dashboard/mitralist/","").then((result:any) => {
+      this.aryListMitra=result.listMitra;
+      console.log("result.listMitra=",result.listMitra)
+  });
     // this.ports = [
     //   { id: 1, name: 'Tokai' },
     //   { id: 2, name: 'Vladivostok' },
@@ -60,22 +87,23 @@ export class Dsh6HomePage {
   }
   ngOnInit() {
     setTimeout(() => {
-      this.dashboarAll.postDatax("Mobile_Dashboard/mitralist/","").then((result:any) => {
-        this.aryListMitra=result.listMitra;
-        console.log("result.listMitra=",result.listMitra);
-      });
+      // this.dashboarAll.postDatax("Mobile_Dashboard/mitralist/","").then((result:any) => {
+      //   this.aryListMitra=result.listMitra;
+      //   console.log("result.listMitra=",result.listMitra)
+      // });
+      document.getElementById("b2s_area").hidden=true;
+      document.getElementById("core_area").hidden=true;
+      document.getElementById("micro_area").hidden=true;
+      document.getElementById("special_area").hidden=true;
+      document.getElementById("card-view").hidden=true;
+      this.initCharting();
+      this.getListMitra();
     }, 100);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Dsh6HomePage');
-    document.getElementById("b2s_area").hidden=true;
-    document.getElementById("core_area").hidden=true;
-    document.getElementById("micro_area").hidden=true;
-    document.getElementById("special_area").hidden=true;
-    document.getElementById("card-view").hidden=true;
-    this.getCharting();
-    this.getListMitra();
+
   }
 
   getListMitra():void{
@@ -91,11 +119,57 @@ export class Dsh6HomePage {
     component: SelectSearchableComponent,
     value: any
   }) {
-      console.log('port:', event.value);
+      console.log('port:', event.value['vendor_id']);
+      this.dashboarAll.postDatax("Mobile_Dashboard/mitralistId/",event.value['vendor_id']).then((result:any) => {
+        this.aryValueMitra=result;
+        //b2s
+        this.aryb2sArea1=result.b2s.area1;
+        this.aryb2sArea2=result.b2s.area2;
+        this.aryb2sArea3=result.b2s.area3;
+        this.aryb2sArea4=result.b2s.area4;
+        //core
+        this.aryMicroArea1=result.cre.area1;
+        this.aryCoreArea2=result.cre.area2;
+        this.aryCoreArea3=result.cre.area3;
+        this.aryCoreArea4=result.cre.area4;
+         //Microcell
+        this.aryMicroArea1=result.mcp.area1;
+        this.aryMicroArea2=result.mcp.area2;
+        this.aryMicroArea3=result.mcp.area3;
+        this.aryMicroArea4=result.mcp.area4;
+         //Special
+        this.arySpecialArea1=result.spr.area1;
+        this.arySpecialArea2=result.spr.area2;
+        this.arySpecialArea3=result.spr.area3;
+        this.arySpecialArea4=result.spr.area4;
+
+        console.log("total_project=",result.total_project);
+        document.getElementById("ttl-prj").innerHTML="Total Project ("+result.total_project+")";
+        document.getElementById("b2s").innerHTML="B2S("+result.total_project+")";
+        document.getElementById("ttl-activity").innerHTML=result.total_activity;
+        document.getElementById("progress-nilai").innerHTML=result.total_progress;
+        document.getElementById("waspang-approval").innerHTML=result.waspang_approval;
+        document.getElementById("waiting-pm-approval").innerHTML=result.waiting_pmapproval;
+        document.getElementById("pm-approval").innerHTML=result.pm_approval;
+
+        //SERIAL
+        setTimeout(() => {
+          this.updateCharting(result);
+        }, 100);
+      });
   }
 
 
+  public viewFlowClick(){
+      let alert1= this.alertCtrl.create({
+        title: '<p>Approval Flow<p>',
+        //subTitle:'subtitle','
+        message: "<img class='home-satelit' src='assets/img/approval_flow.jpeg'>",
+        // cssClass:'modal-flow'
+      });
 
+      alert1.present();
+  }
 
   goToAccount() {
     this.navCtrl.push(SettingsPage);
@@ -164,10 +238,10 @@ export class Dsh6HomePage {
   public setCoreClick(){
     if (clk_core==0){
       document.getElementById("core_area").hidden=false;
-      document.getElementById("core_area1").hidden=true;
-      document.getElementById("core_area2").hidden=true;
-      document.getElementById("core_area3").hidden=true;
-      document.getElementById("core_area4").hidden=true;
+      // document.getElementById("core_area1").hidden=true;
+      // document.getElementById("core_area2").hidden=true;
+      // document.getElementById("core_area3").hidden=true;
+      // document.getElementById("core_area4").hidden=true;
       clk_core=1;
     }else{
       document.getElementById("core_area").hidden=true;
@@ -314,10 +388,55 @@ export class Dsh6HomePage {
     }
   }
 
-  getCharting(){
-    Highcharts.chart({
+  updateCharting(dataSerial:any){
+    //var arySerial=[];
+   // arySerial.push(dataSerial);
+    var a=JSON.stringify(dataSerial.serial);
+    dataSerial.serial.forEach(el=>{
+      console.log("el1=", el.Progress100);
+      console.log("el2=", el.ProgressNull);
+      console.log("el3=", el.Approve);
+      console.log("el4=", el.NeedApproval);
+      console.log("el5=", el.Reject);
+      dsh6_charting.update({
+        series: [{
+            name: 'Brands',
+            // colorByPoint: true,
+            data: [
+              {
+                name: 'Progress Is 100% -',
+                y: el.Progress100,
+                color:'blue',
+                sliced: true,
+                selected: true
+              },{
+                color:'gray',
+                name: 'Progress Is Null',
+                y: el.ProgressNull
+              },{
+                color:'green',
+                name: 'Approve',
+                y:el.Approve
+              },{
+                color:'orange',
+                name: 'Need Approval',
+                y:el.NeedApproval
+              },        {
+                color:'red',
+                name: 'Reject',
+                y:el.Reject
+              }
+            ]
+        }]
+      });
+
+    });
+  }
+
+  initCharting(){
+    dsh6_charting=Highcharts.chart({
       chart: {
-        renderTo:"chartDonut",
+        renderTo:"chart-donut",
         plotBackgroundColor: null,
         plotBorderWidth: null,
         plotShadow: false,
@@ -347,26 +466,26 @@ export class Dsh6HomePage {
           // colorByPoint: true,
           data: [{
             name: 'Progress Is 100% -',
-            y: 0,
+            y: 10,
             color:'blue',
             sliced: true,
             selected: true
           },{
             color:'gray',
             name: 'Progress Is Null',
-            y: 0
+            y: 10
           },{
             color:'green',
             name: 'Approve',
-          y: 0
+            y: 10
           },{
             color:'orange',
             name: 'Need Approval',
-            y: 0
+            y: 10
           },        {
             color:'red',
             name: 'Reject',
-            y: 0
+            y: 10
           }
         ]
       }]
